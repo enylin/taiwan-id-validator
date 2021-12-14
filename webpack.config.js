9cb9a440597974649ff79576b1723881b4e1aa26
Sean Lin <sean.enylin@gmail.com>
2021-12-14 22:46:19 +0800

    fix: make project compatible with es5 environment

--- webpack.config.js
+++ webpack.config.js
@@ -5,6 +5,7 @@ const TerserPlugin = require('terser-webpack-plugin')
 module.exports = {
   mode: 'production',
   devtool: 'source-map',
+  target: ['web', 'es5'],
   entry: {
     index: './src/index.ts',
     'index.min': './src/index.ts'
